29df0949edf0f371f815bac54f5d47e506646d8b
Henry Vo <henry.vo@findex.com.au>
2021-12-20 16:26:29 +1100

    Add first cut of AD service

---
+++ server/ActiveDirectoryService.ts
@@ -0,0 +1,81 @@
+import { config } from "./config";
+
+export class ActiveDirectoryService {
+  tokenEndpoint = config.ACTIVE_DIRECTORY.TOKEN_ENDPOINT;
+
+  async getUserPhoto(userId: string): Promise<string> {
+    const photoPath = `users/${userId}/photo/$value`;
+
+    const accessToken = await this.requestToken();
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    };
+
+    const url = config.ACTIVE_DIRECTORY.GRAPH_ENDPOINT + photoPath;
+
+    try {
+      const response = await this.fetchRetry(url, { method: "get", headers });
+
+      const body: ReadableStream = response.body;
+      const chunks = [];
+
+      for (const chunk of body) {
+        chunks.push(chunk);
+      }
+      return Buffer.concat(chunks).toString("base64");
+    } catch (err) {
+      console.log(`No profile picture found for ${userId}`);
+      throw err;
+    }
+  }
+
+  async fetchRetry(url: string, config: RequestInit): Promise<Response> {
+    return await fetch(url, config);
+  }
+
+  private async requestToken() {
+    const body = this.encodeForm({
+      client_id: config.ACTIVE_DIRECTORY.CLIENT_ID,
+      client_secret: config.ACTIVE_DIRECTORY.CLIENT_SECRET,
+      grant_type: config.ACTIVE_DIRECTORY.CLIENT_CREDENTIALS,
+      scope: config.ACTIVE_DIRECTORY.TOKEN_SCOPE,
+    });
+
+    const requestConfig: RequestInit = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body,
+    };
+
+    console.log(
+      `Active directory service - requesting token from ${this.tokenEndpoint}`
+    );
+    const response = await fetch(this.tokenEndpoint, requestConfig);
+    const responseText = await response.text();
+
+    try {
+      const responseJson = JSON.parse(responseText);
+      const { access_token: accessToken } = responseJson;
+      return accessToken;
+    } catch (error) {
+      console.log("Error decoding response", error);
+      console.log(
+        `Response headers are ${JSON.stringify(
+          response.headers
+        )}, body is ${responseText}`
+      );
+      throw error;
+    }
+  }
+
+  private encodeForm = (data: any) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+}
