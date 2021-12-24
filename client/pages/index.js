eb130ca22b3630a46e8987e0cd864ed2bed6cee3
Henry Vo <henry.vo@findex.com.au>
2021-12-25 09:22:54 +1100

    Homepage touchup

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,17 +8,24 @@ export default function Home({ teams }) {
   return (
     <Layout>
       <main className={styles.main}>
-        <h1 className={styles.title}>select your team</h1>
+        <h1 className={styles.title}>PRESS START</h1>
 
         <div className={styles.grid}>
-          {teams.map((team) => (
-            <Link href={`/teams/${team.id}`} key={team.id}>
-              <a className="focus:ring focus:ring-pink-600 m-4">
+          <div className="flex flex-col border-black border-4 border-solid p-8 pr-14 mt-8">
+            <h2 className="-mt-12 bg-papaya px-4">select your team</h2>
+            {teams.map((team) => (
+              <Link href={`/teams/${team.id}`} key={team.id}>
+                {/* <a className="focus:ring focus:ring-pink-600 m-4">
                 <span className="text-center h-16 flex justify-center items-center w-16 mx-auto mb-4 bg-black text-white align-middle">{team.name[0]}</span>
                 {team.name}
-              </a>
-            </Link>
-          ))}
+              </a> */}
+                <a className="team-select focus:ring focus:ring-pink-600 mt-2">
+                  <span className="team-select__arrow text-2xl mr-2 ">🠶</span>
+                  {team.name}
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       </main>
 
