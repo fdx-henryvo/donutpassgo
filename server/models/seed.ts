11932159f0f63ccccb1f8f66146102c474adc998
Henry Vo <henry.vo@findex.com.au>
2022-01-18 15:22:52 +1100

    Update team members

--- server/models/seed.ts
+++ server/models/seed.ts
@@ -14,7 +14,7 @@ export const data = {
       id: "039343a8-9772-4d9c-ad25-8701a1ac1068",
       name: "Henry Vo",
       teamId: 1,
-      donutCount: 2,
+      donutCount: 1,
     },
     {
       id: "7a5ca07f-31f5-4b60-934e-d69edb7304ef",
@@ -32,13 +32,49 @@ export const data = {
       id: "3a0a25e5-28f1-4f8f-81b5-7152fa1200af",
       name: "Kunal Patel",
       teamId: 1,
-      donutCount: 3,
+      donutCount: 0,
     },
     {
-      id: "b79580cb-2278-4fcb-90aa-60111a1ae980",
-      name: "Nick Day",
+      id: "90d12078-a0be-4a21-80a8-24bbe5328956",
+      name: "James Spielvogel",
       teamId: 1,
-      donutCount: 2,
+      donutCount: 0,
     },
+    {
+      id: "bd610057-b2a9-42e2-91a7-4a9ff889b7b5",
+      name: "Sachit Malhotra",
+      teamId: 1,
+      donutCount: 0,
+    },
+    {
+      id: "fda44a57-9432-40df-bbff-82d002e9ef0a",
+      name: "Peter Fotinis",
+      teamId: 1,
+      donutCount: 0,
+    },
+    {
+      id: "b21d3b64-31bb-41b0-a576-9f2fce335545",
+      name: "Kuan Lee",
+      teamId: 1,
+      donutCount: 0,
+    },
+    {
+      id: "2701e0df-f4f1-4da2-b181-eb4c936c996f",
+      name: "Patricia McCarthy",
+      teamId: 1,
+      donutCount: 0,
+    },
+    {
+      id: "d63615b8-b854-4909-bb02-69b659607bd4",
+      name: "Johnny Le",
+      teamId: 1,
+      donutCount: 0,
+    },
+    {
+      id: "65f4ed87-bb1f-4fd2-b7f6-80e6c0444f2e",
+      name: "Suja Horne",
+      teamId: 1,
+      donutCount: 0,
+    }
   ],
 };
